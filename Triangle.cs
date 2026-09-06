1b77d1c3c76b5e46f0f3f82d8e7b2d9fbec76433
Вероника Ким <veronikakim122020@mail.ru>
2026-09-06 23:33:25 +0700

    созданы классы поинт и траингл

--- Triangle.cs
+++ Triangle.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace WpfApp50
 {
-    public class Triangle
+    public class Triangle лоплпом
     {
         public Point2D P1 { get; private set; }
         public Point2D P2 { get; private set; }
         public Point2D P3 { get; private set; }
-
         public Triangle(Point2D p1, Point2D p2, Point2D p3)
-        { P1 = p1; P2 = p2; P3 = p3; }
-
+        {
+             P1 = p1;
+             P2 = p2;
+             P3 = p3;
+        }
         public void AddX(int x)
         {
             P1.AddX(x);
